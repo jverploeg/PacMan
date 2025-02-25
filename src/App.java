a261bce4995ed1b9a0aca80e0086ba5540795968
jverploeg <verploegjack@gmail.com>
2025-02-25 15:30:53 -0700

    basic window setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+//need to create a window
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //set up window dimensions
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        //create window
+        JFrame frame = new JFrame("PacMan");
+        //make visible
+        frame.setVisible(true);
+        //set window size
+        frame.setSize(boardWidth, boardHeight);
+        //center on screen
+        frame.setLocationRelativeTo(null);
+        //dont allow resize
+        frame.setResizable(false);
+        //terminate on window close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
     }
 }
